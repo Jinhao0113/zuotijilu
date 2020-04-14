565e39342da75011dfb40c1a91fe081c99e1e909
lz2019hjh <3200985003@qq.com>
2020-04-14 19:20:44 +0800

    Fininsh POJ1001

---
+++ poj.org/1000/code.cpp
@@ -0,0 +1,15 @@
+// #include<bits/stdc++.h>
+#include<iostream>
+#include<cmath>
+#include<cstdio>
+#include<cstring>
+
+using namespace std;
+
+int a,b;
+
+int main()
+{
+    while(scanf("%d%d",&a,&b)!=EOF)printf("%d\n",a+b);
+    return 0;
+}
